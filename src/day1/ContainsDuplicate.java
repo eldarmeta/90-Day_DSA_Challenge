e6620b637d88fc5239f43a72ea51a3060bc5aa73
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-06 15:57:44 -0400

    Add Contains Duplicate leetcode java

---
+++ src/day1/ContainsDuplicate.java
@@ -0,0 +1,40 @@
+package day1;
+
+import java.util.Scanner;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Считываем строку чисел с пробелами
+        String[] parts = scanner.nextLine().split(" ");
+        int[] nums = new int[parts.length];
+
+        // Преобразуем в массив int
+        for (int i = 0; i < parts.length; i++) {
+            nums[i] = Integer.parseInt(parts[i]);
+        }
+
+        boolean hasDuplicate = false;
+
+        // Два вложенных цикла для поиска дубликатов
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    hasDuplicate = true;
+                    break; // выйти из внутреннего цикла
+                }
+            }
+            if (hasDuplicate) {
+                break; // выйти из внешнего цикла
+            }
+        }
+
+        // Вывод результата
+        if (hasDuplicate) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
