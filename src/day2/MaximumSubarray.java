96e9bc4cae3e6d5878275d65449a838430f63c16
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-09 23:51:44 -0400

    Add maximum subarray leetcode java

---
+++ src/day2/MaximumSubarray.java
@@ -0,0 +1,31 @@
+package day2;
+
+import java.util.Scanner;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] parts = scanner.nextLine().split(" ");
+        int[] nums = new int[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            nums[i] = Integer.parseInt(parts[i]);
+        }
+
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        System.out.println("Maximum subarray sum: " + maxSum);
+    }
+}
