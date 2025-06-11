e455a7d2b89214aa94d168c38266011ae5fce855
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-10 21:06:10 -0400

    Add single number XOR java code

---
+++ src/day2/singleNumberXOR.java
@@ -0,0 +1,23 @@
+package day2;
+
+import java.util.Scanner;
+
+public class singleNumberXOR {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] parts = scanner.nextLine().split(" ");
+        int[] nums = new int[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            nums[i] = Integer.parseInt(parts[i]);
+        }
+
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;  // XOR-им каждый элемент
+            //result = result ^ num;
+        }
+
+        System.out.println(result);  // выводим уникальное число
+    }
+}
