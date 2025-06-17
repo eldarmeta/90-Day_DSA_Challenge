ce41541b911d3b3eab7cd449b028592f005dbd03
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-17 10:55:23 -0400

    Add Reverse a String java code

---
+++ src/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        String reversed = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversed = reversed + str.charAt(i);
+
+
+        }
+        System.out.println("Reversed: " + reversed);
+    }
+}
+// +++ можно еще освоить StringBuilder — это класс для эффективного построения строк.
+// Он позволяет добавлять символы и строки без создания новых объектов.
+
+//StringBuilder reversed = new StringBuilder(); // создаём пустой буфер
+//reversed.append(str.charAt(i));               // добавляем символ в конец
+
+// В конце: .toString() — преобразует StringBuilder обратно в обычную строку
+//      System.out.println(reversed.toString());
