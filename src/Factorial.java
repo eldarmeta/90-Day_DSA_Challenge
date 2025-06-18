a18d89fa043f2dae13ec354c626046cd31d4e75b
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-17 21:50:33 -0400

    Add factorial n! java code

---
+++ src/Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static void main(String[] args) {
+
+        int num = 5;       // Число, для которого считаем факториал
+        int fact = 1;      // Начинаем с 1 (потому что 0 умножит всё в 0)
+
+        // Идём от 1 до num и умножаем fact на i
+        for(int i = 1; i <= num; i++) {
+            fact *= i;     // fact = fact * i
+        }
+
+        // Выводим результат
+        System.out.println("Factorial: " + fact);
+    }
+}
