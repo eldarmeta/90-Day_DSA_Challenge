89724f0368494f484771004195182a6bfe265ed1
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-18 13:39:45 -0400

    Add prime check optimized java

---
+++ src/PrimeCheckOptimized.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheckOptimized {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int input = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if(input <= 1){
+            isPrime = false;
+        } else {
+          for (int i = 2; i <= Math.sqrt(input); i++){
+              if (input % i == 0){
+                  isPrime = false;
+                  break;
+              }
+          }
+        }
+        if(isPrime){
+            System.out.println(input + " is Prime number!");
+        }else {
+            System.out.println(input + " is not Prime number!");
+        }
+
+    }
+}
