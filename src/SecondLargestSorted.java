983f18302eb81f8b93d668021644fa9cf41938a1
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-18 22:59:46 -0400

    Add second largest sorted v. code

---
+++ src/SecondLargestSorted.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SecondLargestSorted {
+    public static void main(String[] args) {
+        int[] array = {10, 5, 20, 20, 8, 17};
+
+        Arrays.sort(array); // Сортирует по возрастанию
+
+        int largest = array[array.length - 1];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = array.length - 2; i >= 0; i--) {
+            if (array[i] != largest) {
+                secondLargest = array[i];
+                break;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Нет второго уникального наибольшего числа");
+        } else {
+            System.out.println("Второе наибольшее: " + secondLargest);
+        }
+    }
+}
