3ac2d09c321c6be736b10b28f012c683d9e2042a
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-23 21:41:24 -0400

    Add Move zeroes leetcode java

---
+++ src/MoveZeros.java
@@ -0,0 +1,27 @@
+public class MoveZeros {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        // Классический вывод через индексы
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[lastNonZeroIndex];
+                nums[lastNonZeroIndex] = temp;
+
+                lastNonZeroIndex++;
+            }
+        }
+    }
+}
