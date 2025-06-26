fc0c49eca1abc8be5cf0a322fc47882bbec7e6d4
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-26 17:09:31 -0400

    Add Ripple Effect java code

---
+++ src/RippleEffect.java
@@ -0,0 +1,19 @@
+public class RippleEffect {
+
+    public static void main(String[] args) {
+        int waves = 14;
+
+        for (int i = 1; i <= waves; i++) {
+            System.out.println(" ".repeat(waves - i) + "*".repeat(i * 2 - 1));
+            waitFor(500);
+        }
+    }
+
+    public static void waitFor(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
