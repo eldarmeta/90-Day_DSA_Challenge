6ee90d18bfed6967d308fc6fff092ce97a13566e
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-26 17:05:53 -0400

    Add Domino effect java code

---
+++ src/DominoEffect.java
@@ -0,0 +1,19 @@
+public class DominoEffect {
+
+    public static void main(String[] args) {
+        int n = 10; // Количество домино
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Domino " + i + " fell.");
+            waitFor(500); // Пауза, чтобы показать последовательность
+        }
+    }
+
+    public static void waitFor(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
