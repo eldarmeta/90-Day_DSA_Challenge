1a3bb064b211cabf1b534823aa2f80eae796b92c
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-06-27 19:19:01 -0400

    Add relativity java code code

---
+++ src/Relativity.java
@@ -0,0 +1,14 @@
+public class Relativity {
+    public static void main(String[] args) {
+
+        double v = 100000; // скорость объекта (м/с)
+        double c = 300000000; // скорость света (м/с)
+
+        double timeEarth = 60; // 10 секунд на Земле
+        double timeTraveler = timeEarth * Math.sqrt(1 - (v * v) / (c * c));
+
+        System.out.printf("На Земле прошло: %.2f сек\n", timeEarth);
+        System.out.printf("Для путешественника прошло: %.8f сек\n", timeTraveler);
+    }
+}
+
