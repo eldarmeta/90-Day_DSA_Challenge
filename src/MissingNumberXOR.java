ce0fc2254fbd48745dc864f3acdb5222e5575837
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-01 19:51:59 -0400

    Add missing number XOR java code

---
+++ src/MissingNumberXOR.java
@@ -0,0 +1,24 @@
+public class MissingNumberXOR {
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        int missing = missingNumber(nums);
+        System.out.println("Missing number: " + missing);
+    }
+
+    public static int missingNumber(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        // XOR всех чисел от 0 до n
+        for (int i = 0; i <= n; i++) {
+            xor = xor ^ i;
+        }
+
+        // XOR всех элементов в массиве
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+
+        return xor;
+    }
+}
