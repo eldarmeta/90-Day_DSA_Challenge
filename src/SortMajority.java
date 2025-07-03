a79e478af4241842c3a19cd907a63946ac4f2b4e
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-03 14:49:08 -0400

    Add Sort Majority java

---
+++ src/SortMajority.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SortMajority {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        int result = majorityElement(nums);
+        System.out.println("Мажоритарный элемент (Sort): " + result);
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n / 2];
+    }
+}
