60938e77efd847642e5931b6876a8dbd2c501d52
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-13 20:41:52 -0400

    Add valid mountain array java code

---
+++ src/validMountainArray.java
@@ -0,0 +1,27 @@
+public class validMountainArray {
+    public static boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return false;
+
+        int i = 0;
+
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 2, 1};
+        System.out.println("Is valid mountain array? " + validMountainArray(arr)); // true
+    }
+}
