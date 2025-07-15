6badb9a95f1a81aec0fd510ed124c18be4e09295
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-14 22:11:46 -0400

    Add KMP java code

---
+++ src/KMP.java
@@ -0,0 +1,39 @@
+public class KMP {
+    public static int[] buildLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0, i = 1;
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                lps[i++] = ++len;
+            } else {
+                if (len != 0) len = lps[len - 1];
+                else lps[i++] = 0;
+            }
+        }
+        return lps;
+    }
+
+    public static void search(String text, String pattern) {
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++; j++;
+            }
+            if (j == pattern.length()) {
+                System.out.println("🔎 Найдено по индексу " + (i - j));
+                j = lps[j - 1];
+            } else if (i < text.length() && text.charAt(i) != pattern.charAt(j)) {
+                if (j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        search("ababcababcababd", "ababd");
+    }
+}
+
