794c528cebbe488f2662f3f0db8d8a061d247ade
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-19 17:38:14 -0400

    Add Gravity flip from Codeforces (JAVA)

---
+++ src/GravityFlip.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class GravityFlip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();         // Кол-во башен
+        int[] arr = new int[n];       // Массив башен
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();    // Чтение входа
+        }
+
+        Arrays.sort(arr);             // Сортировка башен
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");  // Вывод результата
+        }
+    }
+}
