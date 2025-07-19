2db4d6c7e098f7971fe058d654a7190a2a6bc982
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-19 16:59:35 -0400

    Add 1346. Check If N and Its Double Exist leetcode java (HashSet)

---
+++ src/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckIfNandItsDoubleExist {
+    public static boolean checkIfExist(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int n : arr) {
+            if (seen.contains(2 * n) || (n % 2 == 0 && seen.contains(n / 2))) {
+                return true;
+            }
+            seen.add(n);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {10, 2, 5, 3};      // true
+        int[] test2 = {3, 1, 7, 11};      // false
+        int[] test3 = {0, 0};             // true
+
+        System.out.println(checkIfExist(test1));
+        System.out.println(checkIfExist(test2));
+        System.out.println(checkIfExist(test3));
+    }
+}
