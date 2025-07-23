6ecb47ceb2f39b7ecf6ce56add9b632c5e6c47b0
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-22 23:24:27 -0400

    Add valid palibdrome leetcode 2 pointers java

---
+++ src/Strings/list/day2/ValidPalindromeLeet.java
@@ -0,0 +1,21 @@
+package Strings.list.day2;
+
+public class ValidPalindromeLeet {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(isPalindrome("race a car"));                     // false
+        System.out.println(isPalindrome(" "));                              // true
+        System.out.println(isPalindrome("ab2ba"));                          // true
+    }
+}
