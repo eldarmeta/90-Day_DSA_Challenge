f54c7645cabaf41a1ef2623132667c22f09fb634
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-28 23:05:48 -0400

    Add group anagrams java code

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+// LeetCode 49: Group Anagrams
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> grouped = groupAnagrams(input);
+        System.out.println(grouped);
+    }
+}
