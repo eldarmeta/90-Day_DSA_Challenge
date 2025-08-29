ec1084f403c02da774ff7e4c184be08904c570d2
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-08-28 22:21:24 -0400

    Add two pointers reverse string on java

---
+++ src/Strings/list/day2/twoPointersReverse.java
@@ -0,0 +1,31 @@
+package Strings.list.day2;
+
+import java.util.Scanner;
+
+public class twoPointersReverse {
+
+    // Реверс строки: O(n) по времени, O(n) по памяти (из-за char[])
+    public static String reverse(String input) {
+        if (input == null || input.length() <= 1) return input;
+
+        char[] chars = input.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            char tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
+    // Демонстрация чтения из консоли и печати результата
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(reverse(s));
+        sc.close();
+    }
+}
